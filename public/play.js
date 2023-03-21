be539504d57c48a28a475ef015404a7c36e210b1
Trevor McClellan <tsmcclel@byu.edu>
2023-03-20 21:20:31 -0600

    Updated play page to make requests

--- public/play.js
+++ public/play.js
@@ -125,40 +125,52 @@ class Game {
         return buttons[Math.floor(Math.random() * buttons.length)];
     }
 
-    saveScore(score) {
+    async saveScore(score) {
         const userName = this.getPlayerName();
-        let scores = []
-        const scoresStr = localStorage.getItem('scores');
-        if (scoresStr) {
-            scores = JSON.parse(scoresStr);
-        }
-        scores = this.updateScores(userName, score, scores);
-
-        localStorage.setItem('scores', JSON.stringify(scores));
-    }
-
-    updateScores(userName, score, scores) {
         const date = new Date().toLocaleDateString();
-        const newScore = { userName, score, date };
-
+        const newScore = { name: userName, score: score, date: date };
+    
+        try {
+          const response = await fetch('/api/score', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(newScore),
+          });
+    
+          // Store what the service gave us as the high scores
+          const scores = await response.json();
+          localStorage.setItem('scores', JSON.stringify(scores));
+        } catch {
+          // If there was an error then just track scores locally
+          this.updateScoresLocal(newScore);
+        }
+      }
+    
+      updateScoresLocal(newScore) {
+        let scores = [];
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+          scores = JSON.parse(scoresText);
+        }
+    
         let found = false;
         for (const [i, prevScore] of scores.entries()) {
-            if (score > prevScore.score) {
-                scores.splice(i, 0, newScore);
-                found = true;
-                break;
-            }
+          if (newScore > prevScore.score) {
+            scores.splice(i, 0, newScore);
+            found = true;
+            break;
+          }
         }
-
+    
         if (!found) {
-            scores.push(newScore);
+          scores.push(newScore);
         }
-
+    
         if (scores.length > 10) {
-            scores.length = 10;
+          scores.length = 10;
         }
-
-        return scores;
+    
+        localStorage.setItem('scores', JSON.stringify(scores));
     }
 }
 
